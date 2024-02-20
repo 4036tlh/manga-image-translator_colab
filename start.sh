c78475041b45007cbea680458534fa308b360577
yujianke100 <yujianke100@gmail.com>
2024-02-20 19:49:30 +1100

    update

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
-#/bin/sh
+#!/bin/bash
 name=$1
-python -m manga_translator --verbose --mode batch  --use-gpu --translator=sakura --target-lang=CHS -i ./target/${name}/ -o ./translate_results/${name}_自翻
+if [ ! -d "target" ]; then
+  mkdir "target"
+fi
+if [ ! -d "translate_results" ]; then
+  mkdir "translate_results"
+fi
+python -m manga_translator --verbose --mode batch --use-gpu --translator=sakura --target-lang=CHS -i ./target/${name}/ -o ./translate_results/${name}_自翻
